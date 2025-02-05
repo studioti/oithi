81ee5376571bb13cc4666c2f9bd5d384f197b44f
Thiago Aguiar <thiago.aguiar86@gmail.com>
2025-02-04 23:31:33 -0300

    fix: Correção do cookiebot e correções de css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
-'use client'
-
 import { lazy, Suspense } from 'react'
 import Layout from '../components/Layout'
-// import { Metadata } from 'next'
-import ReactCookieBot from 'react-cookiebot'
+import { Metadata } from 'next'
 
 const About = lazy(() => import('../components/pages/About'))
 const Projects = lazy(() => import('../components/pages/Projects'))
 const Sites = lazy(() => import('../components/pages/Sites'))
 
-// export const metadata: Metadata = {
-//     title: 'Home • Thiago Aguiar • Front-end Developer'
-// }
-
-const domainGroupId = process.env.NEXT_PUBLIC_COOKIEBOT_ID
+export const metadata: Metadata = {
+    title: 'Home • Thiago Aguiar • Front-end Developer'
+}
 
 export default function Index() {
     return (
         <>
-            <ReactCookieBot domainGroupId={domainGroupId} language='PT' />
-            
             <Layout>
-
                 {/* sobre mim */}
                 <Suspense fallback={<div>Carregando ...</div>}>
                     <About />
